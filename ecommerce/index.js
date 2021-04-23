e69499c52f791bdb7b6a3622d5b5c72e986b6fbf
VitalBox_BitBucket <santiago.ortiz@vitalbox.co>
2021-04-22 21:42:11 -0500

    Create the views of the ecommerce using pug

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,9 +3,11 @@ const path = require("path")
 const app = express()
 const productsRouter = require("./routes/products")
 
+/* Configure the view engine */
 app.set("views", path.join(__dirname, "views"))  // Execute in the path where the app exist
 app.set("view engine", "pug")
 
+app.use("/static", express.static(path.join(__dirname, "public")))
 app.use('/products', productsRouter)
 
 // app.listen retorna un servidor
--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/', function(req, res, next) {
-    res.render("products", { products })
+    res.render("products", { products })    // Inject to the view engine
 })
 
 const products = [
